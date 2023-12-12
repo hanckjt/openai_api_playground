4c87357659e702ff75fd900db40f622087abf6f0
Hank <hanckjt@gmail.com>
2023-12-12 18:11:09 +0800

    add concurrency support

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 <img src="https://github.com/hanckjt/openai_api_playground/assets/16874002/ce6eff49-51a9-45ed-936e-70fb3e12137e" width="60%">
 
-The reason why I developed this online application is because I enjoy researching various new LLMs, including open source and closed source online. 
+The reason why I developed this online application is because I enjoy researching various new LLMs, including open source and closed source online.
 However, after deploying the server, I often have to test it. I searched online for a long time but couldn't find a good one, so I wrote one myself with streamlit.
 You can play any API server that compatible with OpenAI API, hoping to help friends with similar needs.
 
@@ -19,7 +19,7 @@ Try it on streamlit cloud: [https://openai-api-playground.streamlit.app/](https:
 - [X] Specify a model and set its parameters
 - [X] Support for stream or non-stream mode
 - [X] Test the connection latency of the API server
-- [X] Test the token generation speed of the model
+- [X] Test the token generation speed of the model with concurrency
 - [X] View the complete session state content of Streamlit
 - [ ] Support multimodal model, including images, audio or video
 - [ ] Conduct Q&A interactions using uploaded text files as a knowledge base
--- pages/2_📊_Test.py
+++ pages/2_📊_Test.py
@@ -1,6 +1,24 @@
 import streamlit as st
 import time
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
 from comm_lib import *
+
+
+def ask_openai(client, model, question):
+    messages = [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": question}]
+    response = client.chat.completions.create(messages=messages, model=model, max_tokens=512, stream=False)
+
+    return response
+
+
+async def get_responses(client, model, questions):
+    with ThreadPoolExecutor() as executor:
+        loop = asyncio.get_event_loop()
+        futures = [loop.run_in_executor(executor, ask_openai, client, model, question) for question in questions]
+        return await asyncio.gather(*futures)
+
 
 def main():
     st.set_page_config(page_title='Test', page_icon='📊', layout='wide', initial_sidebar_state='auto')
@@ -9,7 +27,7 @@ def main():
         st.error('Please set the right API settings first.')
         return
     st.title(f'API Test: {model}')
-    
+
     with st.container(border=True):
         st.write('Base URL Latency')
         cols = st.columns([0.6, 0.2, 0.2])
@@ -20,25 +38,31 @@ def main():
 
     with st.container(border=True):
         st.write('Tokens Generate Speed: Temperature=0.0')
+        cols = st.columns(2)
+        prompt = cols[0].text_input('Prompt', 'Tell me a short joke.')
+        concurrency = cols[1].number_input('Concurrency', 1, 100, 1)
+        prompts = [prompt] * concurrency
+        openai_client = get_openai_client()
         if st.button('Test Speed'):
-            prompt = 'Count from 1 to 20, just numbers, no words.'
             with st.chat_message('user'):
                 st.markdown(prompt)
             with st.spinner('Testing...'):
                 start_time = time.time()
                 try:
-                    response = openai_chat_completion([{'role': 'user', 'content': prompt}], model, False, 128, 0)
+                    responses = asyncio.run(get_responses(openai_client, model, prompts))
                 except Exception as e:
                     st.error(e)
                     return
             end_time = time.time()
-            with st.chat_message('assistant'):
-                st.write(response.choices[0].message.content)
-            tokens = response.usage.completion_tokens
-            speed = tokens / (end_time - start_time)
+            completion_tokens = 0
+            for i, response in enumerate(responses):
+                with st.chat_message('assistant'):
+                    st.write(f'[{i+1}]: {response.choices[0].message.content}')
+                completion_tokens += response.usage.completion_tokens
+            speed = completion_tokens / (end_time - start_time)
             st.write(f'Speed: {speed:.2f} tokens/s')
-            with st.expander('Response'):
-                st.write(response)
+            # with st.expander('Response'):
+            #     st.write(response)
 
 
 if __name__ == "__main__":
