240c6635c820de47ee0221a4582aadf9f92141a0
hank <hanckjt@gmail.com>
2024-04-16 10:46:42 +0800

    1. 改名 0_🏠_Playground.py，去掉表情符； 2. 让run脚本更自动化；

--- run.bat
+++ run.bat
@@ -1,4 +1,16 @@
-@echo off
+ @echo off
 
-REM Run the Streamlit application
-streamlit run 0_🏠_Playground.py
+set VENV_DIR=venv
+set REQUIREMENTS_FILE=requirements.txt
+set PYTHON_SCRIPT=0_Playground.py
+
+if not exist "%VENV_DIR%\" (
+    python -m venv %VENV_DIR%
+
+    if exist "%REQUIREMENTS_FILE%" (
+        %VENV_DIR%\Scripts\pip install -r "%REQUIREMENTS_FILE%"
+    )
+)
+
+%VENV_DIR%\Scripts\streamlit run "%PYTHON_SCRIPT%" %*
+pause
--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-# Run the Streamlit application
-streamlit run 0_🏠_Playground.py
+VENV_DIR="venv"
+REQUIREMENTS_FILE="requirements.txt"
+PYTHON_SCRIPT="0_Playground.py"
+
+if [ ! -d "${VENV_DIR}" ]; then
+    python3 -m venv "${VENV_DIR}"
+
+    if [ -f "${REQUIREMENTS_FILE}" ]; then
+        ${VENV_DIR}/bin/pip install -r "${REQUIREMENTS_FILE}"
+    fi
+fi
+
+sudo "${VENV_DIR}/bin/streamlit run" "${PYTHON_SCRIPT}" "$@"
